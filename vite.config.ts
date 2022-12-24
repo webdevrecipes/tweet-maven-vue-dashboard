dc7a4f6cd7ee331116099491d95a0cf5e27912ff
tom-swokowski <tomswokowski.dev@gmail.com>
2022-12-24 15:50:35 -0600

    Add and config font

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,19 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+import webfontDownload from 'vite-plugin-webfont-dl';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    webfontDownload([
+      'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap',
+    ]),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/assets/scss/global.scss";`,
-      },
     },
   },
 });
